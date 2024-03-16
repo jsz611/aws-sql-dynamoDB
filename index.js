cd3759cb668b598766ca7aa35f6698e8687e62c1
Josiel Souza <souzajosiel611@gmail.com>
2024-03-16 13:59:42 -0300

    feat: add dynamoDB

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-import { config } from 'dotenv';
-config();
-import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
-import express from 'express';
-import serverless from 'serverless-http';
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import AWS from "aws-sdk";
+import serverless from "serverless-http";
+
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 const app = express();
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+const queueUrl = process.env.SQS_QUEUE_URL;
+
 app.use(express.json());
 
+app.post("/transaction", async (req, res) => {
+  const { idempotencyId, amount, type } = req.body;
 
+  const messageDeduplicationId = idempotencyId;
+
+  const params = {
+    MessageBody: JSON.stringify({ idempotencyId, amount, type }),
+    QueueUrl: queueUrl,
+    MessageGroupId: "YourMessageGroupId",
+    MessageDeduplicationId: messageDeduplicationId,
+  };
+
+  try {
+    const sendMessageResult = await sqs.sendMessage(params).promise();
+    res.json({
+      message: "Transação enviada para a fila SQS com sucesso!",
+      MessageId: sendMessageResult.MessageId,
+    });
+  } catch (error) {
+    console.error("Erro ao enviar mensagem para a fila SQS:", error);
+    res.status(500).send("Erro ao enviar a transação para a fila SQS");
+  }
+});
+
+AWS.config.logger = console;
+
+// Para executar localmente, descomente a linha abaixo:
 const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Bem-vindo à API!');
+app.listen(port, () => {
+  console.log(`API rodando em http://localhost:${port}`);
 });
 
-  
-const sqsClient = new SQSClient({ region: 'us-east-1' });
-const queueUrl = 'https://sqs.us-east-1.amazonaws.com/533267034838/fila-desafio-aws.fifo';
-app.post('/transaction', async (req, res) => {
-    const { idempotencyId, amount, type } = req.body;
-  
-    const params = {
-        QueueUrl: queueUrl,
-        MessageBody: JSON.stringify({ idempotencyId, amount, type }),
-        MessageGroupId: 'TransactionGroup', 
-        MessageDeduplicationId: idempotencyId, 
-    };
-  
-    console.log('Enviando mensagem para a fila SQS:', params);
-
-    try {
-        const command = new SendMessageCommand(params);
-        const data = await sqsClient.send(command);
-        console.log('Mensagem enviada com sucesso, ID:', data.MessageId);
-        res.json({ message: 'Transação enviada para a fila com sucesso!', messageId: data.MessageId });
-    } catch (error) {
-        console.error('Erro ao enviar mensagem para a fila SQS:', error);
-        res.status(500).send('Erro ao enviar a transação para a fila');
-    }
-});
-
-
- app.listen(port, () => {
-  console.log(`Aplicação rodando na porta ${port}`);
-});  
+// Para executar na AWS Lambda, use o seguinte:
 export const handler = serverless(app);
