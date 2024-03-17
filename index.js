e43547c7273a440c785ce732f04a331511157925
Josiel Souza <souzajosiel611@gmail.com>
2024-03-17 11:11:30 -0300

    feat: add serveless http

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-import { config } from "dotenv";
-config();
-import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import express from "express";
+import AWS from "aws-sdk";
 import serverless from "serverless-http";
-//import AWS from 'aws-sdk';
 
-const app = express();
-app.use(express.json());
-
-//const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Bem-vindo à API!");
+AWS.config.update({
+  region: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-const sqsClient = new SQSClient({ region: "us-east-1" });
-const queueUrl =
-  "https://sqs.us-east-1.amazonaws.com/533267034838/fila-desafio-aws.fifo";
-app.post("/transaction", async (req, res) => {
-  const { idempotencyId, amount, type } = req.body;
+const app = express();
+const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
+const queueUrl = process.env.SQS_QUEUE_URL;
 
+app.use(express.json());
+
+app.post("/transaction", async (req, res) => {
+  const { id, name } = req.body;
   const params = {
+    MessageBody: JSON.stringify({ id: Number(id), name }),
     QueueUrl: queueUrl,
-    MessageBody: JSON.stringify({ idempotencyId, amount, type }),
-    MessageGroupId: "TransactionGroup",
-    MessageDeduplicationId: idempotencyId,
+    MessageGroupId: "YourMessageGroupId",
+    MessageDeduplicationId: id.toString(),
   };
 
-  console.log("Enviando mensagem para a fila SQS:", params);
-
   try {
-    const command = new SendMessageCommand(params);
-    const data = await sqsClient.send(command);
-    console.log("Mensagem enviada com sucesso, ID:", data.MessageId);
+    const sendMessageResult = await sqs.sendMessage(params).promise();
     res.json({
-      message: "Transação enviada para a fila com sucesso!",
-      messageId: data.MessageId,
+      message: "Mensagem enviada para a fila SQS com sucesso!",
+      MessageId: sendMessageResult.MessageId,
     });
   } catch (error) {
     console.error("Erro ao enviar mensagem para a fila SQS:", error);
-    res.status(500).send("Erro ao enviar a transação para a fila");
+    res.status(500).send("Erro ao enviar a mensagem para a fila SQS");
   }
 });
 
-/* app.listen(port, () => {
-  console.log(`Aplicação rodando na porta ${port}`);
-});  */
+// Configuração para execução local
+const port = 3000;
+app.listen(port, () => {
+  console.log(`API rodando em http://localhost:${port}`);
+});
+
+// Configuração para execução como função Lambda
 export const handler = serverless(app);
---
+++ test-script.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const url =
+  "https://5z50kcp9u4.execute-api.us-east-1.amazonaws.com/default/teste-banco-message";
+
+async function sendTransaction(id, name) {
+  try {
+    const response = await axios.post(url, { id, name });
+    console.log(`Transação ${id} enviada com sucesso.`);
+    return response;
+  } catch (error) {
+    console.error(
+      `Erro ao enviar a transação ${id}:`,
+      error.response ? error.response.data : error.message
+    );
+  }
+}
+
+async function sendTransactions() {
+  for (let i = 1; i <= 100; i++) {
+    await sendTransaction(i, `Transação ${i}`);
+  }
+  console.log("Script concluído.");
+}
+
+sendTransactions();
