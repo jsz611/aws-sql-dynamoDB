ef4f0ac36f082b5efc5c5ed332a98d7f958c6b21
Josiel Souza <souzajosiel611@gmail.com>
2024-03-17 20:22:48 -0300

    feat: change deploy

--- test/test-script.js
+++ test/test-script.js
@@ -1,22 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'https://5z50kcp9u4.execute-api.us-east-1.amazonaws.com/default/teste-banco-message';
+const url =
+  "https://5z50kcp9u4.execute-api.us-east-1.amazonaws.com/default/teste-banco-message";
 
-async function sendTransaction(id, name) {
+async function sendTransaction(idempotencyId, amount, type) {
   try {
-    const response = await axios.post(url, { id, name });
-    console.log(`Transação ${id} enviada com sucesso.`);
+    const response = await axios.post(url, { idempotencyId, amount, type });
+    console.log(`Transação ${idempotencyId} enviada com sucesso.`);
     return response;
   } catch (error) {
-    console.error(`Erro ao enviar a transação ${id}:`, error.response ? error.response.data : error.message);
+    console.error(
+      `Erro ao enviar a transação ${idempotencyId}:`,
+      error.response ? error.response.data : error.message
+    );
+    throw error;
   }
 }
 
 async function sendTransactions() {
   for (let i = 1; i <= 100; i++) {
-    await sendTransaction(i, `Transação ${i}`);
+    await sendTransaction(i, `Valor ${i}`, "debit");
   }
-  console.log('Script concluído.');
+  console.log("Script concluído.");
 }
 
-sendTransactions();
+sendTransactions().catch((e) => {
+  console.error("Um erro ocorreu durante o envio das transações", e);
+});
