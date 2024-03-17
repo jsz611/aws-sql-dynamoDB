cfa072eebc0cec4a2980546c3d55898c1b6a6d0e
Josiel Souza <souzajosiel611@gmail.com>
2024-03-17 15:39:14 -0300

    feat: add deploy

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import express from "express";
-import AWS from "aws-sdk";
-import serverless from "serverless-http";
+import dotenv from "dotenv";
 
-AWS.config.update({
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
+dotenv.config();
 
 const app = express();
-const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
-const queueUrl = process.env.SQS_QUEUE_URL;
-
 app.use(express.json());
+
+const sqsClient = new SQSClient({ region: process.env.AWS_REGION });
 
 app.post("/transaction", async (req, res) => {
   const { id, name } = req.body;
   const params = {
     MessageBody: JSON.stringify({ id: Number(id), name }),
-    QueueUrl: queueUrl,
+    QueueUrl: process.env.SQS_QUEUE_URL,
     MessageGroupId: "YourMessageGroupId",
     MessageDeduplicationId: id.toString(),
   };
 
   try {
-    const sendMessageResult = await sqs.sendMessage(params).promise();
+    const sendMessageResult = await sqsClient.send(
+      new SendMessageCommand(params)
+    );
     res.json({
       message: "Mensagem enviada para a fila SQS com sucesso!",
       MessageId: sendMessageResult.MessageId,
@@ -35,11 +32,7 @@ app.post("/transaction", async (req, res) => {
   }
 });
 
-// Configuração para execução local
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API rodando em http://localhost:${port}`);
 });
-
-// Configuração para execução como função Lambda
-export const handler = serverless(app);
